1100a35d7ef3eadf9578fe230f7afb506f049e24
Camilo Barbosa <barbosa93cg@gmail.com>
2017-05-20 04:08:32 -0500

    update 9

---
+++ bin/game/includes/MiMapa.h
@@ -0,0 +1,28 @@
+#ifndef MIMAPA_H
+#define MIMAPA_H
+
+
+#include "game.h"
+
+
+
+class MiMapa : public mapa
+{
+public:
+    /** Default constructor */
+    MiMapa();
+    /** Default destructor */
+    virtual ~MiMapa();
+
+    virtual void empezar();
+    virtual void mientras(int mils);
+    virtual void fin();
+
+
+
+protected:
+    actor * a1,*a2;
+private:
+};
+
+#endif // MIMAPA_H
---
+++ bin/game/sources/MiMapa.cpp
@@ -0,0 +1,34 @@
+#include "MiMapa.h"
+
+
+using namespace std;
+
+
+MiMapa::MiMapa()
+{
+    //ctor
+
+}
+
+MiMapa::~MiMapa()
+{
+    //dtor
+}
+
+
+void MiMapa::empezar()
+{
+       
+    mapa::empezar();
+}
+
+
+void MiMapa::mientras(int mils)
+{
+    mapa::mientras(mils);
+}
+
+void MiMapa::fin()
+{
+    mapa::fin();
+}
